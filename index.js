e095b57257970b1e1bb3b62681731d27729b515e
Balkishan Jaipal <98956626+BalkishanJaipal@users.noreply.github.com>
2022-06-02 08:49:33 +0530

    1

---
+++ index.js
@@ -0,0 +1,75 @@
+function validate()
+{
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+   
+     var cpassword = document.getElementById('cpassword').value;
+    
+    checkUserName(username);
+    checkemail(email);
+    checkPassword(password);
+    checkPasswordMatch(password ,cpassword);
+   
+    
+}
+
+function checkUserName(username)
+{
+    if(username.length>7)
+    {   
+        document.getElementById('username').classList.add('success');
+        document.getElementById('username').classList.replace('error','success');
+        document.getElementById('username_error').innerText = ''
+    }
+    else{
+        document.getElementById('username').classList.add('error');
+        document.getElementById('username_error').innerText = 'Username must be 8 letters long'
+    }
+}
+function checkemail(email)
+{
+    if(email.length>8 && email.includes('@gmail'))
+    {   
+        document.getElementById('email').classList.add('success');
+        document.getElementById('email').classList.replace('error','success');
+        document.getElementById('email_error').innerText = ''
+    }
+    else{
+        document.getElementById('email').classList.add('error');
+        document.getElementById('email_error').innerText = 'Email must include @gmail ,& 8 letters long'
+        
+        
+
+    }
+}
+
+
+function checkPassword(password)
+{
+    if(password.length>7 && password.includes('@'))
+    {  
+        document.getElementById('password').classList.add('success');
+        document.getElementById('password').classList.replace('error','success');
+        document.getElementById('password_error').innerText = ''
+    }
+    else{
+        document.getElementById('password').classList.add('error');
+        document.getElementById('password_error').innerText = 'Password must include @ and 8 letters long'
+    }
+}
+
+
+function checkPasswordMatch(password,cpassword)
+{
+    if(password!='' && password==cpassword)
+    {   
+        document.getElementById('cpassword').classList.add('success');
+        document.getElementById('cpassword').classList.replace('error','success');
+        document.getElementById('cpassword_error').innerText = ''
+    }
+    else{
+        document.getElementById('cpassword').classList.add('error');
+        document.getElementById('cpassword_error').innerText = 'Password not matched'
+    }
+}
